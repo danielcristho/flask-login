a743c526c3277c78511cf7413ac6e3bdff2b7730
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-02 22:14:31 +0700

    add: static files

--- app/auth.py
+++ app/auth.py
@@ -51,3 +51,14 @@ def login():
         template = 'login-page',
         body = "Log in with your User account."
     )
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth_bp.login'))
---
+++ routes.py
@@ -0,0 +1,29 @@
+from crypt import methods
+from turtle import title
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_required, logout_user
+
+#Blueprint configuration
+main_bp = Blueprint(
+    'main_bp', __name__,
+    template_folder = 'templates',
+    static_folder = 'static'
+)
+
+@main_bp.route('/', methods=['GET'])
+@login_required
+def dashboard():
+    return render_template('dashboard.jinja',
+        title = 'Flask-Login',
+        template = 'dashboard-template',
+        current_user = current_user,
+        body = "You are now logged in"
+    )
+
+@main_bp.route("/logout")
+@login_required
+def logaout():
+    logout_user()
+    return redirect(url_for('auth_bp.login'))
+
+
